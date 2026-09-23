be9d5d564a3a873d4d2c6bf93971b14c5498633d
Anjali Rathore <130468761+vishuanjali@users.noreply.github.com>
2026-09-23 15:33:43 +0530

    Add AWS security group configuration with ingress and egress rules

---
+++ SecurityGroup.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "rathore-sg" {
+  name        = "terraform-sg"
+  description = "Security group created using Terraform"
+  vpc_id      = "vpc-01f1cadf9bae98f60"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Terraform-SG"
+  }
+}
